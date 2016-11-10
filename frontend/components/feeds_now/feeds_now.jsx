3339299165e6e87c259a2eef9547ae8306c3ec30
fan222 <sfwahahaha222@gmail.com>
2016-11-10 09:55:01 -0800

    add desc to feeds now

--- frontend/components/feeds_now/feeds_now.jsx
+++ frontend/components/feeds_now/feeds_now.jsx
@@ -49,16 +49,19 @@ class FeedsNow extends React.Component {
 
   render() {
     if (Object.keys(this.props.feeds).length === 1) {
-      return <div className="sk-cube-grid">
-                <div className="sk-cube sk-cube1"></div>
-                <div className="sk-cube sk-cube2"></div>
-                <div className="sk-cube sk-cube3"></div>
-                <div className="sk-cube sk-cube4"></div>
-                <div className="sk-cube sk-cube5"></div>
-                <div className="sk-cube sk-cube6"></div>
-                <div className="sk-cube sk-cube7"></div>
-                <div className="sk-cube sk-cube8"></div>
-                <div className="sk-cube sk-cube9"></div>
+      return  <div className="laoding">
+                <h1 className="loading-desc">Thank you for waiting.</h1>
+                <div className="sk-cube-grid">
+                  <div className="sk-cube sk-cube1"></div>
+                  <div className="sk-cube sk-cube2"></div>
+                  <div className="sk-cube sk-cube3"></div>
+                  <div className="sk-cube sk-cube4"></div>
+                  <div className="sk-cube sk-cube5"></div>
+                  <div className="sk-cube sk-cube6"></div>
+                  <div className="sk-cube sk-cube7"></div>
+                  <div className="sk-cube sk-cube8"></div>
+                  <div className="sk-cube sk-cube9"></div>
+                </div>
               </div>;
     } else {
       return (
